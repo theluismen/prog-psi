5594ba52ae5d5fefcdb7c2e5d3c7af4ca6e0e8b4
acabrerrob <acabrerrob@educacion.navarra.es>
2025-12-01 14:23:17 +0100

    Constructor usuarisPDI

--- grup4_Pract3/src/usuaris/UsuariPDI.java
+++ grup4_Pract3/src/usuaris/UsuariPDI.java
@@ -2,5 +2,20 @@ package usuaris;
 
 public class UsuariPDI extends Usuari{
 
-    
+    private String departament;
+    private String campus;
+
+    /**
+     * Constructor UsuariPDI.
+     * @param alies Alias de la persona
+     * @param email
+     * @param collectiu
+     * @param department
+     * @param campus
+     */
+    public UsuariPDI(String alies, String email, String collectiu, String department, String campus){
+        super(alies, email, "PDI");
+        this.departament = department;
+        this.campus = campus;
+    }
 }
