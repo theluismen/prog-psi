986e1d9e01a8aa0403ebfa69469d30004c45ae1a
aaesshhhh <aeshanz36@gmail.com>
2025-12-01 11:13:03 +0100

    commit2

---
+++ grup4_Pract3/src/activitats/Activitat.java
@@ -0,0 +1,60 @@
+import java.time.LocalDate;
+
+public abstract class Activitat {
+
+    protected String nom;
+    protected String[] collectius;
+    protected LocalDate dataIniInscripcio;
+    protected LocalDate dataFiInscripcio;
+
+    public Activitat (String nom; String[] collectius, LocalDate dataIniInscripcio, LocalDate dataFiInscripcio) {
+        this.nom = nom;
+        this.collectius = collectius;
+        this.dataIniInscripcio = dataIniInscripcio;
+        this.dataFiInscripcio = dataFiInscripcio;
+    }
+
+    //Getters
+
+    public String getNom() {    return nom;  }
+
+    public String[] getcollectius() {   return collectius;  }
+
+    public LocalDate getDataIniInscripcio()  {  return dataIniInscripcio;  }
+
+    public LocalDate getDataFiInscripcio()  {  return dataFiInscripcio;  }
+
+
+    // Funcions base
+
+    /**
+     * Mètode que determina si avui es pot fer una inscripcio
+     * 
+     * @param avui la data d'aquell dia
+     * @return true si es pot fer la inscripcio, sinó false
+     */
+    public boolean esEnPeriodeInscripcio(LocalDate avui) {
+        return (avui.isEqual(dataIniInscripcio) || (avui.isAfter((dataIniInscripcio))) 
+                && avui.isBefore(dataFiInscripcio) || (avui.isBefore(dataFiInscripcio)));
+
+    }
+    
+    public abstract boolean estaActiva (LocalDate avui);
+
+    public abstract String tipusActivitat();
+
+    @Override
+    public String toString() {
+        String info = "Nom: " + nom + "\nCol·lectius: ";
+
+        for (int i = 0; i < colectius.length; i++) {
+            info += colectius[i];
+            if (i < colectius.length - 1) info += ", ";
+        }
+
+        info += "\nPeríode inscripció: " + dataIniciInscripcio +
+                " fins " + dataFiInscripcio + "\n";
+
+        return info;
+    }
+}
---
+++ grup4_Pract3/src/usuaris/Usuari.java
@@ -0,0 +1,59 @@
+public abstract class Usuari {
+    
+    protected String alies;           //Identificador del usuari
+    protected String collectiu;      // Part principal del correu (sense @)
+    protected String email;           // PDI / PTGAS / Estudiant
+
+
+
+    //Constructor
+    public Usuari (String alies; String email, String collectiu) {
+        this.alies = alies;
+        this.email; email;
+        this.collectiu = collectiu;
+    }
+
+
+    //Getters
+    public String getAlies() {  return alies;  }
+
+    public String getEmailComplet() {
+        //Segons el tipus de col·lectiu: URV o PDI i PTGAS
+        if (collectiu.equalsIgnoreCase("Estudiant")) {
+            return email + "@estudiants.urv.cat";
+        } else {
+            return email + "@urv.cat";  //PDI i PTGAS
+        }
+    }
+
+    public String getCollectiu() {  return collectiu;  }
+
+
+    /**
+     * Mètode que retorna informació extra de cada tipus d'usuari
+     * 
+     * Estudiant -> ensenyament + any inici
+     * PDI -> departament + campus
+     * PTGAS -> campus
+     * 
+     * @return informació específica de la subclasse
+     */
+    public abstract String dadesExtra();
+
+    /**
+     * Mètode que retorna el tipus d'usuari (Estudiant, PDI, PTGAS)
+     * 
+     * @return String del tipus d'Usuari
+     */
+    public abstract String tipusUsuari();
+
+    @Override
+    public String toString() {
+        return "Àlies: " + alies + 
+                "\nCorreu: " + getEmailComplet(); + 
+                "\nCol·lectiu: " + colectiu +
+                "\n" + dadesExtra() + "\n";
+    }
+
+    
+}
