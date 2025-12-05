ba185e25ecf86812d5e5d29330c46b339a1478bb
theluismen <luismaocampo28@gmail.com>
2025-12-05 15:39:54 +0100

    feat: archivo AppConsola

---
+++ grup4_Pract3/src/apps/AppConsola.java
@@ -0,0 +1,12 @@
+/**
+ * Autor(@s):   Ainara Sofia Cabrera Robles
+                Ikram Kheira Hallouz Safa
+                Aesha Naz Mahmood Bibi
+                Alexandra Núñez González
+ * Descripción: 
+ */
+public class AppConsola {
+    public static void main(String[] args) {
+
+    }
+}
