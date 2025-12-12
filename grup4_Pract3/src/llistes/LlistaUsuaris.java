d92c5b0909b788f348e33a778d1caddf5281d569
aaesshhhh <aeshanz36@gmail.com>
2025-12-12 09:40:56 +0100

    LlistaUsuaris.java (Excepcions afegides + estructura arreglada)

--- grup4_Pract3/src/llistes/LlistaUsuaris.java
+++ grup4_Pract3/src/llistes/LlistaUsuaris.java
@@ -1,15 +1,16 @@
+//Aesha Naz
 package llistes;
-import enumeraciones.CampusURV;
+import enumeraciones.*;
+import excepcions;
+import usuaris.*;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import usuaris.*;
-
-
 
 public class LlistaUsuaris {
     
-    private Usuari[] usuaris;   //array q conté els usuaris
+    private Usuari[] usuaris;   //array que conté els usuaris
     private int compt;          //nombre d'usuaris guardats
 
     /**
@@ -20,25 +21,32 @@ public class LlistaUsuaris {
         compt = 0;
     }
 
-    //Metodes
+    /**
+     *  Retorna el nombre d'usuaris guardats a la llista
+     * 
+     * @return nombre d'usuaris
+     */
+    public int getNumUsuaris() {
+        return compt;
+    }
 
     /**
      * Afegeix un usuari a la llista de forma ordenada pel seu alies
      * No es poden afegir duplicats
      * 
      * @param u usuari que es vol llegir 
-     * @throws IllegalArgumentException si ja existeix un usuari amb el mateix alies
+     * @throws UsuariDuplicat excepcio si ja existeix un usuari amb el mateix alies
      */
-    public void afegirUsuari(Usuari u) {
+    public void afegirUsuari(Usuari u) throws UsuariDuplicat {
         if(existeix(u.getAlies())) {
-            throw new IllegalArgumentException( "ERROR: Ja existeix un usuari amb aquest àlies " + u.getAlies() );
+            throw new UsuariDuplicat(u.getAlies());
         }
 
         if (compt == usuaris.length) {
             ampliar();
         }
 
-        //Insercio ordenada per alies
+        //Inserció ordenada per alies
         int pos = compt - 1;
         while(pos >= 0 && usuaris[pos].getAlies().compareTo(u.getAlies()) > 0) {
             usuaris[pos + 1] = usuaris[pos];
@@ -46,9 +54,7 @@ public class LlistaUsuaris {
         }
         usuaris[pos + 1 ] = u;
         compt++;
-
     }
-
 
     /**
      * Cerca un usuari pel seu alies
@@ -63,7 +69,6 @@ public class LlistaUsuaris {
             }
         }
         return null;
-        
     }
 
     /**
@@ -73,30 +78,17 @@ public class LlistaUsuaris {
      * @return true si la cerca retorna un usuari diferent de null sino false
      */
     public boolean existeix(String alies) {
-        Usuari trobat = cerca(alies);
-        return trobat != null;
+        return cerca(alies) != null;
     }
-
-
-    /**
-     *  Retorna el nombre d'usuaris guardats a la llista
-     * 
-     * @return nombre d'usuaris
-     */
-    public int getNumUsuaris() {
-        return compt;
-    }
-
 
     /**
      * Metode que mostra per pantalla tots els usuaris de la llista
      */
     public void mostrarU() {
         for(int i = 0; i < compt; i++) {
-            System.out.println(usuaris[i]);
+            System.out.println(usuaris[i] + "\n");
         }
     }
-
 
     /**
      * Metode que mostra els usuaris que pertanyen a un col·lectiu indicat
@@ -106,60 +98,83 @@ public class LlistaUsuaris {
     public void mostrarUCollectiu(String col) {
         for(int i = 0; i < compt; i++) {
             if(usuaris[i].getCollectiu().equalsIgnoreCase(col)) {
-                System.out.println(usuaris[i]);
+                System.out.println(usuaris[i] + "\n");
             }
         }
     }
-
-
 
     /**
-     * Metode que carrega usuaris des d'un fitxer de text, cada línia representa un usuari que separa les dades amb ";"
-     * i segons el col·lectiu es crearà l'objecte 
+     * Metode que carrega usuaris des d'un fitxer de text
      * 
-     * @param nomFitxer nom del fitxer a carregar
-     * @throws IOException excepcio si hi han problemes de lectura del fitxer
-     * @throws IllegalArgumentException excepcio per si hi ha un col·lectiu desconegut
+     * 
+     * @param nomFitxer nom del fitxer
+     * @throws IOException excepcio si no es pot llegir el fitxer
+     * @throws FormatInvalid excepcio si el format és incorrecta
+     * @throws CollectiuDesconegut excepcio si el col·lectiu no és reconegut
+     * @throws UsuariDuplicat excepcio si un àlies ja existeix
      */
-    public void carregaFitxer(String nomFitxer) throws IOException  {
-        BufferedReader br = new BufferedReader(new FileReader(nomFitxer));
-        String linia;
+    public void carregaFitxer(String nomFitxer) throws IOException, FormatInvalid, CollectiuDesconegut, UsuariDuplicat  {
+        try (BufferedReader br = new BufferedReader(new FileReader(nomFitxer))) {
+            String linia;
 
-        while  ((linia = br.readLine()) != null) {
-            String[] dades = linia.split(";");
+        
+            while  ((linia = br.readLine()) != null) {
 
-            String alies = dades[0];
-            String email = dades[1];
-            String col = dades[2];
+                String[] dades = linia.split(";");
 
-            Usuari u;
+                if (dades.length < 3) {
+                    throw new FormatInvalid("Falten camps obligatoris a la línia: " + linia);
+                }
 
-            switch (col) {
-                case "Estudiant":
-                    u = new UsuariEstudiant(alies, email, dades[3], Integer.parseInt(dades[4]));
-                    break;
-                case "PDI":
-                    u = new UsuariPDI(alies, email, dades[3], dades[4]);
-                    break;
-                case "PTGAS":
-                    CampusURV campus = CampusURV.fromString(dades[3]);
-                    u = new UsuariPTGAS(alies, email, campus);
-                    break;
-                default:
-                    throw new IllegalArgumentException("Col·lectiu desconegur: " + col);
+                String alies = dades[0];
+                String email = dades[1];
+                String col = dades[2];
+
+                Usuari u;
+
+                switch (col) {
+
+                    case "Estudiant":
+                        if (dades.length != 5){
+                            throw new FormatInvalid("Format uncorrecte per Estudiant: " + linia);
+                        }
+
+                        String ensenyament = dades[3];
+                        u = new UsuariEstudiant(alies, email, ensenyament, Integer.parseInt(dades[4]));
+                        break;
+
+                    case "PDI":
+
+                    if (dades.length != 5){
+                            throw new FormatInvalid("Format uncorrecte per PDI: " + linia);
+                        }
+                        DepartamentURV dept = DepartamentURV.valueOf(dades[3]);
+                        CampusURV campPDI = CampusURV.valueOf(dades[4]);
+                    
+                        u = new UsuariPDI(alies, email, col, dept, campPDI);
+                        break;
+
+                    case "PTGAS":
+
+                        if (dades.length != 4){
+                            throw new FormatInvalid("Format uncorrecte per PTGAS: " + linia);
+                        }
+
+                        CampusURV campusPTGAS = CampusURV.valueOf(dades[3]);
+                        u = new UsuariPTGAS(alies, col, email, campusPTGAS);
+                        break;
+                        
+                    default:
+                        throw new CollectiuDesconegut("Col·lectiu desconegut: " + col);
 
 
+                }
+                afegirUsuari(u);
             }
-
-            afegirUsuari(u);
-        }
-
         br.close();
+        }
     }
  
-
-
-    //Metode auxiliar
     /**
      * Metode que duplica la capacitat de l'array d'usuaris si d'arriba al limit
      */
