2f4111028686f4c85b537fc7197406ae580cee61
aaesshhhh <aeshanz36@gmail.com>
2025-12-30 17:45:21 +0100

    Controlador Formulari

---
+++ grup4_Pract3/src/gui/ControladorFormulari.java
@@ -0,0 +1,57 @@
+package gui;
+
+import activitats.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import llistes.LlistaActivitats;
+
+public class ControladorFormulari implements ActionListener {
+
+    /* Atributs de la classe */
+    private final JCheckBox checkOnline;
+    private final JCheckBox checkPeriodica;
+    private final JCheckBox checkUnDia;
+
+    private LlistaActivitats llistaActivitats;
+    
+    /** Constructor */
+    public ControladorFormulari (JCheckBox checkOnline, JCheckBox checkPeriodica, 
+        JCheckBox checkUnDia, LlistaActivitats llistaActivitats) {
+            
+            this.checkOnline = checkOnline;
+            this.checkPeriodica = checkPeriodica;
+            this.checkUnDia = checkUnDia;
+        }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        boolean mostrarOnline = checkOnline.isSelected();
+        boolean mostrarPeriodica = checkPeriodica.isSelected();
+        boolean mostrarUnDia = checkUnDia.isSelected();
+
+        System.out.println("=== FILTRE ACTIVITATS ===");
+        System.out.println("Activitat Online: " + mostrarOnline);
+        System.out.println("Activitat Periòdica: " + mostrarPeriodica);
+        System.out.println("Activitat Un dia: " + mostrarUnDia);
+
+        //Comprovem que el filtratge funciona
+        for (int i = 0; i < llistaActivitats.getNumElements(); i++) {
+            try {
+                Activitat act = llistaActivitats.getActivitatReal(i);
+
+                if (act instanceof ActivitatOnline && mostrarOnline) {
+                    System.out.println("ACTIVITAT ONLINE → " + act.getNom());
+                } else if (act instanceof  ActivitatPeriodica && mostrarPeriodica) {
+                    System.out.println("ACTIVITAT PERIODICA → " + act.getNom());
+                } else if (act instanceof ActivitatUnDia && mostrarUnDia) {
+                    System.out.println("ACTIVITAT UN DIA → " + act.getNom());
+                }
+
+            } catch (Exception exc) {
+                //No hauria de passar
+            }
+        }
+    }
+}
